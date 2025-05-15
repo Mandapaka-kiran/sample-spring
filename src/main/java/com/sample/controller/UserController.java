80f445e76d4e62ca5534192d81147c0f932f0efe
sai <dhavileswarapusaikumar@gmail.com>
2025-05-16 00:58:06 +0530

    Find All Api Created.

---
+++ src/main/java/com/sample/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.sample.controller;
+
+import com.sample.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserController {
+    @Autowired
+    UserService userService;
+    @GetMapping("/all")
+    public ResponseEntity getAllUsers(){
+        return new ResponseEntity(userService.findAll(), HttpStatus.FOUND);
+    }
+}
---
+++ src/main/java/com/sample/entity/User.java
@@ -0,0 +1,45 @@
+package com.sample.entity;
+
+import lombok.Builder;
+
+@Builder
+
+public class User {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String name;
+    private String username;
+    private String email;
+}
