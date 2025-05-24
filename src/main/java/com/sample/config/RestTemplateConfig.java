c791c8df4939a999185439aa6377028240b93d5d
sai <dhavileswarapusaikumar@gmail.com>
2025-05-25 00:34:41 +0530

    added test meethods for controller

--- src/main/java/com/sample/config/RestTemplateConfig.java
+++ src/main/java/com/sample/config/RestTemplateConfig.java
@@ -10,13 +10,13 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
 
     @Bean
-    public RestTemplate restTemplate1(){
+    public RestTemplate restTemp(){
         return new RestTemplate();
     }
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
+//    @Bean
+//    public PasswordEncoder passwordEncoder(){
+//        return new BCryptPasswordEncoder();
+//    }
     //security Implementation
 
 }
