9b73c1397abcf0bc11bbedc9eb83964b79a20faa
sai <dhavileswarapusaikumar@gmail.com>
2025-05-24 12:52:31 +0530

    dev-branch changes

--- src/main/java/com/sample/config/RestTemplateConfig.java
+++ src/main/java/com/sample/config/RestTemplateConfig.java
@@ -1,15 +1,17 @@
 package com.sample.config;
 
+import jakarta.servlet.FilterChain;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+
 public class RestTemplateConfig {
 
     @Bean
-    public RestTemplate restTemplate(){
+    public RestTemplate restTemplate() {
         return new RestTemplate();
     }
-
 }
+
