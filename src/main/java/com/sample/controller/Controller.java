5772f078f00857b90de37e8e93acb443a9a30555
sai <dhavileswarapusaikumar@gmail.com>
2025-06-03 23:12:51 +0530

    added getUserBy PathVariable

--- src/main/java/com/sample/controller/Controller.java
+++ src/main/java/com/sample/controller/Controller.java
@@ -5,10 +5,7 @@ import com.sample.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -20,8 +17,8 @@ public class Controller {
     public ResponseEntity test(){
         return ResponseEntity.status(HttpStatus.FOUND).body("Welcome");
     }
-    @GetMapping("/getUser")
-    public ResponseEntity getByUserBy(@RequestParam("id") int id){
+    @GetMapping("/getUser/{id}")
+    public ResponseEntity getUserByPathVariable(@PathVariable("id") int id){
             User user= userService.getUSerById(id);
         return
 
